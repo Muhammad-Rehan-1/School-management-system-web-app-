6e6545c5b314c30fd8412e173b305b5adfee7d1b
Muhammad-Rehan-1 <shaheemurrehan2005@gmail.com>
2026-02-25 00:51:55 +0500

    Reset migration history to PostgreSQL for Render

--- server/prisma/migrations/20260103175440_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Student" ADD COLUMN "address" TEXT;
-ALTER TABLE "Student" ADD COLUMN "classGrade" TEXT;
-ALTER TABLE "Student" ADD COLUMN "contact" TEXT;
-ALTER TABLE "Student" ADD COLUMN "email" TEXT;
-ALTER TABLE "Student" ADD COLUMN "enrollmentDate" DATETIME;
-ALTER TABLE "Student" ADD COLUMN "father" TEXT;
-ALTER TABLE "Student" ADD COLUMN "gender" TEXT;
